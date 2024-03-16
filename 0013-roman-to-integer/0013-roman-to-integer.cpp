e592d8018b5796a848cc55cb45084baf423fcebf
Anit Sarkar <anitsarkar04@gmail.com>
2024-03-16 23:27:21 +0530

    Time: 8 ms (65.96%), Space: 12.7 MB (55.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -7,12 +7,13 @@ public:
         unordered_map<char, int> m = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         for(int i=0;i<s.length();i++)
         {
-            if(m[s[i]]<m[s[i+1]])
+            if(m[s[i]]<m[s[i+1]]){
                 sum=sum - m[s[i]]; // when a smaller weight symbol comes before a larger weight symbol  ...example IV =  5-1 so we subtract 1 
-
+            }
             
-            else
+            else{
                 sum=sum +m[s[i]]; 
+            }
         }
         return sum;
     }
