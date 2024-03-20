e8aee519cbb760aec11d723f5fcd99c99e0034e7
Anit Sarkar <anitsarkar04@gmail.com>
2024-03-20 23:02:38 +0530

    Time: 5 ms (19.49%), Space: 8.1 MB (24.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   int sq_sum(int n){
+          int k, sum=0;
+        while(n !=0){
+            k=n%10;
+            sum =sum+k*k;
+            n=n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        
+        unordered_set<int>s;
+        while(s.find(n)==s.end()){
+            s.insert(n);
+           n= sq_sum( n);
+            if(n==1){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+};
