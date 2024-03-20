e10dcab5836cf1cd76ef4220b51f4fabed0a4d9b
Anit Sarkar <anitsarkar04@gmail.com>
2024-03-20 23:12:37 +0530

    Time: 0 ms (100.00%), Space: 7 MB (89.14%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-   int sq_sum(int &n){
+   //int sq_sum(int &n){//this is also correct
+    int sq_sum(int n){
           int k, sum=0;
         while(n !=0){
             k=n%10;
@@ -9,6 +10,7 @@ public:
         }
         return sum;
     }
+    /* This is also correct
     bool isHappy(int n) {
         
         unordered_set<int>s;
@@ -21,5 +23,21 @@ public:
             
         }
         return false;
+    }*/
+    bool isHappy(int n) {//in this method the &n upper not working 
+       int slow=n;
+        int first=sq_sum(n);
+        if(slow==1|| first==1){
+            return true;
+        }
+        while(slow !=first){
+            slow=sq_sum(slow);
+            first=sq_sum(sq_sum(first));
+             if(slow==1|| first==1){
+            return true;
+        }
+        }
+        return false;
+        
     }
 };
