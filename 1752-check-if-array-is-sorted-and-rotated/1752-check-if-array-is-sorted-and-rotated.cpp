390c5a99deb0481702cdcefde431bbe25f0f7e57
Anit Sarkar <anitsarkar04@gmail.com>
2024-07-05 18:25:52 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (23.30%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -3,6 +3,13 @@ public:
     bool check(vector<int>& nums) {
         int n=nums.size();
         int count=0;
+        //NOTE:When i equals n-1 (the last iteration of the loop), nums[i+1] attempts to access nums[n], which is out of bounds since array indices in C++ are 0-based and go from 0 to n-1. Accessing nums[n] tries to access an element beyond the end of the array, leading to undefined behavior and potentially a runtime error. for this reason for(int i=0;i<n;i++){
+           // if(nums[i]>nums[i+1]){
+           //     count++;
+           // }---->not working
+             
+        
+        
         for(int i=1;i<n;i++){
             if(nums[i-1]>nums[i]){
                 count++;
