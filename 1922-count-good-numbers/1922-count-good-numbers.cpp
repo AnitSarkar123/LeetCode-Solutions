58c7b4ea31b28f88de6ab159ba7471ce7a76d6b9
Anit Sarkar <anitsarkar04@gmail.com>
2024-08-26 21:15:56 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (32.33%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     int mod=1e9+7;
+    int binExpo(long long x,long long n){
+        int ans=1;
+        while(n){
+            if(n&1){
+                ans=(ans*x)%mod;
+        n--;
+            }
+            x=(x*x)%mod;
+            n>>=1;
+        }
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+       long long ans =binExpo(20,n/2)%mod;
+        if(n%2){
+            ans =(ans*5)%mod;
+        }
+        return ans;
+    }
+};
